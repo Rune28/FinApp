c954ab9acbfd9857b67a909e8300cd4515baecfd
Alex Ovchinnikov Windows <alexrune8@gmail.com>
2020-06-23 17:07:22 +0300

    Revert to f5d6209

---
+++ Investment/config_iex.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 16:42:25 2020
+
+@author: aovch
+"""
+
+
+iex_token = "sk_1427456e783248ec8841be2292d08a7d"
---
+++ config_telegram.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 16:45:59 2020
+
+@author: aovch
+"""
+
+token_telegram =  '1166987527:AAF6t4J0TY-tTOJNEzxcxYDts27vqQjdDCM'
