1414cb713ad4eac0330c693f5fcf5f3390843026
Alex Ovchinnikov Windows <alexrune8@gmail.com>
2020-06-26 09:57:18 +0300

    Fix tickers error

--- Database/load_tickers.py
+++ Database/load_tickers.py
@@ -5,7 +5,7 @@ Created on Mon Jun 22 18:07:14 2020
 @author: aovch
 """
 import json
-from Database.DBHelper import finapp_stocks
+from DBHelper import finapp_stocks
 
 def stocks_from_file(js):
     values_tickers = [tuple(i.values()) for i in js]
