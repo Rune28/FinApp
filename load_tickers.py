15a69b3cbfd995cee3367564000c220080d9d0e5
Alex Ovchinnikov Windows <alexrune8@gmail.com>
2020-06-26 10:06:13 +0300

    Fix ticker load

--- load_tickers.py
+++ load_tickers.py
@@ -6,12 +6,15 @@ Created on Mon Jun 22 18:07:14 2020
 """
 import json
 from Database.DBHelper import finapp_stocks
+import os
+
+database_file = os.path.join(os.getcwd(),'Database','All_tickers.txt')
 
 def stocks_from_file(js):
     values_tickers = [tuple(i.values()) for i in js]
     return tuple(values_tickers)
 
-with open('All_tickers.txt', 'r') as fh:
+with open(database_file, 'r') as fh:
     file = json.loads(fh.read())
 
 tuples = stocks_from_file(file)
